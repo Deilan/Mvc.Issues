c14621c6c2d6340824a4df0393a42f324c6cf77b
Deilan <deilan@users.noreply.github.com>
2018-08-20 15:21:08 +0300

    prepare a sample for https://github.com/aspnet/Mvc/issues/8292

---
+++ Mvc.Issues.N8292/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Issues.N8292.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(GetQuery query)
+        {
+            return new[] { "value1", "value2" };
+        }
+    }
+
+    public class GetQuery
+    {
+        [FromQuery]
+        public string Filter { get; set; }
+        [FromQuery]
+        public string Sort { get; set; }
+        [FromQuery]
+        public string Page { get; set; }
+        [FromQuery]
+        public string PageSize { get; set; }
+    }
+}
---
+++ Mvc.Issues.N8292/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Mvc.Issues.N8292
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
